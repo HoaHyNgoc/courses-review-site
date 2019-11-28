1e811ef1ff5ed05a21f88d539047ab631018e709
unknown <hyhoa2000@gmail.com>
2019-11-28 12:55:52 +0700

    create component courese detail page

--- academics-review-app/src/App.js
+++ academics-review-app/src/App.js
@@ -2,13 +2,15 @@ import React from "react";
 import GlobalNavigation from "./components/global_page_navigation/GlobalNavigation";
 import ComponentsPageCourses from "./components/page_courses/ComponentsPageCourses";
 import GlobalFooter from "./components/global_page_footer/GlobalFooter";
+import ComponentsPageCoursesDetail from "./components/page_courses_detail/ComponentsPageCoursesDetail";
 
 function App() {
   return (
     <div className="super_container">
-      <GlobalNavigation />
-      <ComponentsPageCourses />
-      <GlobalFooter />
+        <GlobalNavigation />
+        {/* <ComponentsPageCourses /> */}
+        <ComponentsPageCoursesDetail />
+        <GlobalFooter />
     </div>
   );
 }
---
+++ academics-review-app/src/components/page_courses_detail/ComponentsPageCoursesDetail.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import PageCoursesHeader from '../page_courses/child_page_courses/PageCoursesHeader';
+
+class ComponentsPageCoursesDetail extends Component {
+    render() {
+        return (
+            <div>
+                <PageCoursesHeader />
+            </div>
+        );
+    }
+}
+
+export default ComponentsPageCoursesDetail;
