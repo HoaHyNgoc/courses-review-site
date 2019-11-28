0363de7a8165f792e1334d753199cee4edbff508
unknown <hyhoa2000@gmail.com>
2019-11-28 12:41:11 +0700

    modify background header courses page

--- academics-review-app/src/components/page_courses/child_page_courses/PageCoursesHeader.js
+++ academics-review-app/src/components/page_courses/child_page_courses/PageCoursesHeader.js
@@ -3,14 +3,14 @@ import React, { Component } from "react";
 class PageCoursesHeader extends Component {
   render() {
     return (
-      <div className="home">
-        <div className="home_background_container prlx_parent">
-            <div className="home_background prlx" style={{ backgroundImage: "url(images/courses_background.jpg)" }}></div>
+        <div className="home">
+          <div className="home_background_container prlx_parent">
+              <div className="hero_slide_background" style={{backgroundImage: 'url(images/slider_background.jpg)'}} />
+          </div>
+          <div className="home_content">
+              <h1>Courses</h1>
+          </div>
         </div>
-        <div className="home_content">
-          <h1>Courses</h1>
-        </div>
-      </div>
     );
   }
 }
