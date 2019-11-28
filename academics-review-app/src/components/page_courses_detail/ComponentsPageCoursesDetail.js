767c5ce14b7eeac960e97a85ba8f66761735bf75
unknown <hyhoa2000@gmail.com>
2019-11-28 13:06:34 +0700

    create component detail content courses

--- academics-review-app/src/components/page_courses_detail/ComponentsPageCoursesDetail.js
+++ academics-review-app/src/components/page_courses_detail/ComponentsPageCoursesDetail.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import PageCoursesHeader from '../page_courses/child_page_courses/PageCoursesHeader';
+import CoursesDetailContent from './child_page_courses_detail/CoursesDetailContent';
 
 class ComponentsPageCoursesDetail extends Component {
     render() {
         return (
             <div>
                 <PageCoursesHeader />
+                <CoursesDetailContent />
             </div>
         );
     }
---
+++ academics-review-app/src/components/page_courses_detail/child_page_courses_detail/CoursesDetailContent.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class CoursesDetailContent extends Component {
+    render() {
+        return (
+            <div className="news">
+                <div className="container">
+                    <div className="row">
+                        <h1>This is content courses detail</h1>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default CoursesDetailContent;
