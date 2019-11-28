a3d0e8c5f35cd4b059d4dccf3fa72f426da8125d
unknown <hyhoa2000@gmail.com>
2019-11-28 16:08:57 +0700

    adding title section in detail courses page

--- academics-review-app/src/components/page_courses_detail/child_page_courses_detail/DetailContentSection.js
+++ academics-review-app/src/components/page_courses_detail/child_page_courses_detail/DetailContentSection.js
@@ -2,15 +2,18 @@ import React, { Component } from 'react';
 import DetailContentSubSection from './DetailContentSubSection';
 
 class DetailContentSection extends Component {
+    SectionContentTitle = (idCourse, titleSection) => {
+        return (
+            <div className="news_post_title">
+                <a href="news_post.html">{titleSection}</a>
+            </div>
+        );
+    };
+
     render() {
         return (
             <div className="col-lg-8 scroll-add">
-                <DetailContentSubSection />
-                <DetailContentSubSection />
-                <DetailContentSubSection />
-                <DetailContentSubSection />
-                <DetailContentSubSection />
-                <DetailContentSubSection />
+                {this.SectionContentTitle("1", "Title Section")}
                 <DetailContentSubSection />
                 <DetailContentSubSection />
                 <DetailContentSubSection />
