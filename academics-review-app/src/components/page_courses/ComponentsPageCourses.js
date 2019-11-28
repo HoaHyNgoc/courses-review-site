48466c916134631035857c07ea2c71db6b4fa969
unknown <hyhoa2000@gmail.com>
2019-11-28 11:00:23 +0700

    create folder components and component page courses

---
+++ academics-review-app/src/components/page_courses/ComponentsPageCourses.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ComponentsPageCourses extends Component {
+    render() {
+        return (
+            <div>
+                <h1>This course page</h1>
+            </div>
+        );
+    }
+}
+
+export default ComponentsPageCourses;
